553fda2098f9edd0c9f6898170bcc23212103c91
Berrie Zielman <berrie.zielman@gmail.com>
2016-06-25 19:07:31 +0000

    version 1.1

---
+++ R/hmap.R
@@ -0,0 +1,25 @@
+hmap<-function(x,dominance=FALSE,...)
+{
+  dendrogram <- NULL
+  Rowv <- NULL
+  Colv <- NULL
+  trace <- NULL
+  if(!is.matrix(x))     stop("first argument should be a matrix")
+  if (nrow(x)!=ncol(x)) stop("the same number of rows and columns are expected")
+  #  order//
+  x <- .5*(x-t(x))
+  bin<-x*0
+  bin[x>0]<-1
+  bin[x<0]<--1
+  rsbin<-rowSums(bin)
+  x<-x[order(rsbin),order(rsbin)]
+  #  keuze tussen binair en original
+  if(dominance)
+  {
+    bin <- bin[order(rsbin),order(rsbin)]
+    heatmap.2(bin,dendrogram = 'none', Rowv=FALSE, Colv=FALSE,trace='none', ...)
+  }
+  else {
+    heatmap.2(x,dendrogram = 'none', Rowv=FALSE, Colv=FALSE,trace='none', ...)
+  }
+}
