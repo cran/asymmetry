20291bb723094b63648e8af3d4ad6bb2376b721c
Berrie Zielman <berrie.zielman@gmail.com>
2016-12-04 13:52:33 +0000

    version 1.1.3

--- R/decomposition.R
+++ R/decomposition.R
@@ -1,8 +1,10 @@
-decomposition <- function(X)
+decomposition <- function(x)
 {
-  S <-.5*(t(X)+X)
-  A <-.5*(X - t(X))
-  lk <- list(S = S,A = A)
+  if (nrow(x)!=ncol(x)) stop("the same number of rows and columns are expected")
+  S <-.5*(t(x)+x)
+  A <-.5*(x - t(x))
+  lin = colMeans(A)
+  lk <- list(S = S, A = A, linear = lin)
   class(lk) <- "decomposition"
   lk
 }
--- R/slidevector.R
+++ R/slidevector.R
@@ -1,11 +1,11 @@
 slidevector <-
-function(data,dim=2,itmax=125,eps=1e-12){
+function(data,dim=2,verbose=FALSE,itmax=125,eps=1e-12){
   if (sum(data<0) > 0) stop("data for the slide-vector model should be positive")
 #check for number of rows equals number of columns
   if (nrow(data)!=ncol(data)) stop("the same number of rows and columns are expected")
 
     hjw <- .jnew("asymmetry/slidevector")     # create instance of slidevector class
-  dis <- .jcall(hjw, "[[D","slidevectormodel",.jarray(data,dispatch=TRUE),as.integer(dim),as.integer(itmax),as.double(eps))
+  dis <- .jcall(hjw, "[[D","slidevectormodel",.jarray(data,dispatch=TRUE),as.integer(dim),verbose,as.integer(itmax),as.double(eps))
   ndim <- .jcall(hjw,"I","getDimension")
   nobs <- .jcall(hjw,"I","getNobs")
   niter <- .jcall(hjw,"I","getNiter")
